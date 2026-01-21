196450a318d8d6622bbed409b767d3bc78611f65
Sergio Cordoba <sergiocordoba246@gmail.com>
2026-01-21 20:54:00 +0000

    Entrega final

--- src/app.py
+++ src/app.py
@@ -112,56 +112,190 @@ def get_users_favorites():
 
     return jsonify({
         "planets": [
-            fav.planet.planet_informacion()
-            for fav in favorites.favorites_planet
+            favorites.planet.planet_informacion()
+            for favorites in favorites.favorites_planet
         ],
         "characters": [
-            fav.character.mostrar_informacion()
-            for fav in favorites.favorites_character
+            favorites.character.mostrar_informacion()
+            for favorites in favorites.favorites_character
         ],
         "starships": [
-            fav.starship.starship_informacion()
-            for fav in favorites.favorites_starship
+            favorites.starship.starship_informacion()
+            for favorites in favorites.favorites_starship
         ]
     }), 200
 
 
 
 
-# # [POST] /favorite/planet/<int:planet_id> Añade un nuevo planet favorito al usuario actual con el id = planet_id.
-# @app.route('/people', methods=['POST'])
-# def all_people():
-#     return
+# [POST] /favorite/planet/<int:planet_id> Añade un nuevo planet favorito al usuario actual con el id = planet_id.
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    user_id = 1
 
+    # Verificamos que tenemos el usuario actual como dije el ejercicio
+    user = db.session.get(User, user_id)
+    if not user:
+        return jsonify({"details": "user not found"}), 404
+    
+    
+    # Verificamos el id del planeta
+    planet =db.session.get(Planet, planet_id)
+    if not planet:
+        return jsonify({"details": "planet not found"}), 400
+    
 
+    # Agregamos el favorites
+    favorites = user.favorites
+    if not favorites:
+        favorites = Favorites(user_id=user_id)
+        db.session.add(favorites)
+        db.session.commit()
+    else:
+        favorites = favorites[0]    
+
+    # Una vez tengamos el favorites, le dicimos al sistema que SI YA EXISTE no lo vayas a duplicar
+    existing_favorite = db.session.execute(select(FavoritePlanet).where(
+            FavoritePlanet.favorites_id == favorites.id,
+            FavoritePlanet.planet_id == planet_id
+            )).scalar_one_or_none()
+
+    if existing_favorite:
+        return jsonify({"details": "planet already in favorites"}), 400
+
+    
+    #Agregamos el favorito
+    favorite_planet = FavoritePlanet(
+        favorites_id=favorites.id,
+        planet_id=planet_id
+    )
+
+    db.session.add(favorite_planet)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Planet added to favorites",
+        "planet": planet.planet_informacion()
+    }), 201   
 
 
 # # [POST] /favorite/people/<int:people_id> Añade un nuevo people favorito al usuario actual con el id = people_id.
-# @app.route('/people', methods=['POST'])
-# def all_people():
-#     return
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_people(people_id):
+    user_id = 1
+
+    # Verificamos que tenemos el usuario actual como dije el ejercicio
+    user = db.session.get(User, user_id)
+    if not user:
+        return jsonify({"details": "user not found"}), 404
+    
+    
+    # Verificamos el id del people
+    people =db.session.get(Character, people_id)
+    if not people:
+        return jsonify({"details": "character not found"}), 400
+    
+
+    # Agregamos el favorites
+    favorites = user.favorites
+    if not favorites:
+        favorites = Favorites(user_id=user_id)
+        db.session.add(favorites)
+        db.session.commit()
+    else:
+        favorites = favorites[0]    
+
+    # Una vez tengamos el favorites, le decimos al sistema que SI YA EXISTE no lo vayas a duplicar
+    existing_favorite = db.session.execute(select(FavoriteCharacter).where(
+            FavoriteCharacter.favorites_id == favorites.id,
+            FavoriteCharacter.character_id == people_id
+            )).scalar_one_or_none()
+
+    if existing_favorite:
+        return jsonify({"details": "character already in favorites"}), 400
+
+    
+    #Agregamos el favorito
+    favorite_character = FavoriteCharacter(
+        favorites_id=favorites.id,
+        character_id=people_id
+    )
+
+    db.session.add(favorite_character)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Character added to favorites",
+        "character": people.mostrar_informacion()
+    }), 201      
 
 
 
 
-# # [DELETE] /favorite/planet/<int:planet_id> Elimina un planet favorito con el id = planet_id.
-# @app.route('/people', methods=['DELETE'])
-# def all_people():
-#     return
+# [DELETE] /favorite/planet/<int:planet_id> Elimina un planet favorito con el id = planet_id.
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+
+    user_id = 1
+
+    user = db.session.get(User, user_id)
+    if not user:
+        return jsonify({"details": "user not found"}), 404
+    
+    favorites = user.favorites
+    if not favorites:
+        return jsonify({"details": "no favorites found for user"}), 404
+    else:
+        favorites = favorites[0]
+
+    existing_favorite = db.session.execute(select(FavoritePlanet).where(
+        FavoritePlanet.favorites_id == favorites.id,
+        FavoritePlanet.planet_id == planet_id)
+    ).scalar_one_or_none()
+
+    if not existing_favorite:
+        return jsonify({"details": "planet not found in favorites"}), 404
+
+    db.session.delete(existing_favorite)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Planet removed from favorites"
+    })    
 
 
 
 
 # # [DELETE] /favorite/people/<int:people_id> Elimina un people favorito con el id = people_id.
-# @app.route('/people', methods=['GET'])
-# def all_people():
-#     return
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(people_id):
 
+    user_id = 1
 
+    user = db.session.get(User, user_id)
+    if not user:
+        return jsonify({"details": "user not found"}), 404
+    
+    favorites = user.favorites
+    if not favorites:
+        return jsonify({"details": "no favorites found for user"}), 404
+    else:
+        favorites = favorites[0]
 
+    existing_favorite = db.session.execute(select(FavoriteCharacter).where(
+        FavoriteCharacter.favorites_id == favorites.id,
+        FavoriteCharacter.character_id == people_id)
+    ).scalar_one_or_none()
 
+    if not existing_favorite:
+        return jsonify({"details": "character not found in favorites"}), 404
 
+    db.session.delete(existing_favorite)
+    db.session.commit()
 
+    return jsonify({
+        "message": "Character removed from favorites"
+    })    
 
 
 # this only runs if `$ python src/app.py` is executed
